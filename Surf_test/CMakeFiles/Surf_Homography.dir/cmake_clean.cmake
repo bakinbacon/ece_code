54b4786b4f07c99f547560aaf72ca85d864cdec5
bakinbacon <matt@Matts-MacBook-Air.local>
2015-03-30 22:32:41 -0400

    Updated Alex's code with Surf Feature Detection.
    Right now the Surf uses pepsi cans as the object to detect but working
    on other objects

---
+++ Surf_test/CMakeFiles/Surf_Homography.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Surf_Homography.dir/Surf_Homography.cpp.o"
+  "Surf_Homography.pdb"
+  "Surf_Homography"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Surf_Homography.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
